63aeec8496edbc854ae9aaeb3e3e1544e5b5776b
mahdisalarabedi <122230899+mahdisalarabedi@users.noreply.github.com>
2025-05-19 12:24:20 +0330

    Update Extracting_Conversations.py

--- Extracting_Conversations.py
+++ Extracting_Conversations.py
@@ -1,6 +1,6 @@
 def Conversation_extractor(number_of_conversations):    
     import json
-    with open(r'result.json',"r",encoding="utf-8") as f: 
+    with open(r'result.json',"r",encoding="utf-8") as f:
         data=json.load(f)
 
     def create_dict_of_beloved_keys(dict,key_to_extract):   
